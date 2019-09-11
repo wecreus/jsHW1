14beba6e0cf240ffd54241633130e994f84d83c6
wecreus <wecreus@gmail.com>
2019-09-11 14:06:58 +0300

    easyest part is done

--- script.js
+++ script.js
@@ -1,89 +1,22 @@
-function checkSingleInput() {
-    let value = this.value;
-    if(value.length > 3){
-        if(!this.nextSibling.classList.contains('normalCheckMark')){
-            this.nextSibling.className = 'normalCheckMark';
-        }
-    } else {
-        if(this.nextSibling.classList.contains('normalCheckMark')){
-            this.nextSibling.classList.remove('normalCheckMark');
-        }
+let a, b;
+
+function evaluateNumbers() {
+    a = document.getElementById("a").value;
+    b = document.getElementById("b").value;
+
+    // converting to number so then we can check if it is an integer
+    a = Number(a);
+    b = Number(b);
+
+    if(((a >= 2 && b >= 2) && (a <= 10 && b <= 10)) && (Number.isInteger(a) && Number.isInteger(b))){
+        doSomething();
     }
 }
 
-function checkEmail() {
-    let valueIsBiggerThanThree = this.value.length > 3;
-    let hasSpecialChar = false;
-    for(let i = 0; i < this.value.length; i++){
-        if(this.value[i] === "@"){hasSpecialChar = true;}
-    }
-    if(valueIsBiggerThanThree === true && hasSpecialChar === true){
-        if(!this.nextSibling.classList.contains('normalCheckMark')){
-            this.nextSibling.className = 'normalCheckMark';
-        }
-    } else {
-        if(this.nextSibling.classList.contains('normalCheckMark')){
-            this.nextSibling.classList.remove('normalCheckMark');
-        }
-    }
+
+function doSomething(){
+    console.log("a = " + a);
+    console.log("b = " + b);
+    console.log(typeof a);
+    console.log(typeof b);
 }
-
-function checkMultipleInputs(divElementFromArray) {
-    function checkIfArrayHasFalse(array) {
-        let result = false;
-        array.forEach(function(element){
-            if(element === false) {
-                result = true;
-            }
-        });
-        return result;
-    }
-
-    let arrayOfInputs = divElementFromArray.getElementsByTagName('input');
-    let arrayOfValidInputs = [];
-    for(let i = 0; i < arrayOfInputs.length; i++){
-        arrayOfValidInputs.push(false);
-    }
-
-    function changeImage(value){
-        let storingImg = arrayOfInputs[arrayOfInputs.length - 1].nextSibling;
-        if(value === false){
-            if(!storingImg.classList.contains('normalCheckMark')){
-                storingImg.className = 'normalCheckMark';
-            }
-        } else {
-            if(storingImg.classList.contains('normalCheckMark')){
-                storingImg.classList.remove('normalCheckMark');
-            }
-        }
-    }
-
-    for(let i = 0; i < arrayOfInputs.length; i++){
-        arrayOfInputs[i].addEventListener('input', function(){
-            arrayOfValidInputs[i] = this.value.length > 3;
-            changeImage(checkIfArrayHasFalse(arrayOfValidInputs));
-            }, false);
-    }
-}
-
-document.onreadystatechange = () => {
-    if (document.readyState === 'complete') {
-        document.getElementById('rank').addEventListener('input', checkSingleInput, false);
-        document.getElementById('name').addEventListener('input', checkSingleInput, false);
-        document.getElementById('surname').addEventListener('input', checkSingleInput, false);
-        document.getElementById('street').addEventListener('input', checkSingleInput, false);
-
-        // working with divs that have a couple of inputs
-        let arrayOfDivs = document.getElementsByClassName('coupleInputs');
-        for(let i = 0; i < arrayOfDivs.length; i++){
-            checkMultipleInputs(arrayOfDivs[i]);
-        }
-
-        document.getElementById('bd').addEventListener('input', checkSingleInput, false);
-        document.getElementById('email2').addEventListener('input', checkEmail, false);
-
-
-    }
-};
-
-
